ae60894a908d68cf6f9ea2cf3574e65f0dc526bb
BCR <lukebrainard1226@gmail.com>
2022-01-26 19:56:30 -0800

    Dice challenge
    question 1 code for dice challenge

--- diceChallenge.java
+++ diceChallenge.java
@@ -1,3 +1,12 @@
-public static void main(system [] args){
-  
+public class diceChallenge{
+    public static void main(String args[]){
+        int rolls[] = new int[20];
+        for(int i = 0; i < 20; i++){
+            rolls [i] = ((int)(Math.random()*6))+1;
+        }
+        for(int i = 0; i<20; i++){
+            System.out.println("Roll " + (i+1) + ": " +  rolls[i]);
+        }
+    }
 }
+
