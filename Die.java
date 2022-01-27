393a082deda616518f6a9485ecabdff070c9b4cf
BCR <lukebrainard1226@gmail.com>
2022-01-26 20:35:11 -0800

    Die class
    Die class for question 2

--- Die.java
+++ Die.java
@@ -1,3 +1,28 @@
 public class Die{
+    private int numSides;
+    private int rolledNum;
+    public Die(){
 
+    }
+    public Die(int sidesOfDie){
+        setNumSides(sidesOfDie);
+    }
+    public int numSides(){
+        return numSides;
+    }
+    public int roll(){
+        rolledNum = (int)(Math.random()*numSides) + 1;
+        return rolledNum;
+    }
+    public int readLastRoll(){
+        if(rolledNum > 0){
+            return rolledNum;
+        }
+        else{
+            return -1;
+        }
+    }
+    public void setNumSides(int sides){
+        numSides = sides;
+    }
 }
--- main.java
+++ main.java
@@ -1,1 +1,15 @@
-
+public class main{
+    public static void main(String[] args){
+        Die dice[] = new Die[50];
+        int totalDiceValue = 0;
+        for(int i = 0; i<50; i++){
+            dice[i].setNumSides(6);
+            dice[i].roll();
+            System.out.println("Roll " + i + ": " + dice[i].readLastRoll());
+        }
+        for(int i = 0; i<50; i++){
+            totalDiceValue += dice[i].readLastRoll();
+        }
+        System.out.println("Total value of the " + dice.length + " rolls is " + totalDiceValue);
+    }
+}
