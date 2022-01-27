18860f7daf2dd6f9d2d1d4770acf0220d3d25623
BCR <lukebrainard1226@gmail.com>
2022-01-26 20:46:21 -0800

    Update to die class
    updates to the Die class and the main class that finishes up and adds comments to both.

--- Die.java
+++ Die.java
@@ -1,19 +1,26 @@
 public class Die{
+    //Stores number of sides die has
     private int numSides;
+    //Store what number the die has rolled
     private int rolledNum;
+    //Basic die constructor
     public Die(){
 
     }
+    //Die constructor when inputed the sides of the die
     public Die(int sidesOfDie){
         setNumSides(sidesOfDie);
     }
+    //Returns the number of sides the die has
     public int numSides(){
         return numSides;
     }
+    //Rolls the die and stores it in rolled num. rolled num is returned
     public int roll(){
         rolledNum = (int)(Math.random()*numSides) + 1;
         return rolledNum;
     }
+    //Returns the last roll given by the user to the program if rolled num does not have a value then -1 is returned
     public int readLastRoll(){
         if(rolledNum > 0){
             return rolledNum;
@@ -22,6 +29,7 @@ public class Die{
             return -1;
         }
     }
+    //sets the num of sides the die has
     public void setNumSides(int sides){
         numSides = sides;
     }
--- mainClass.java
+++ mainClass.java
@@ -1,15 +1,20 @@
 public class mainClass{
     public static void main(String[] args){
+        //Creates an array of dices
         Die dice[] = new Die[50];
+        //variable storing the total dice value
         int totalDiceValue = 0;
+        //for loop that initalizes each index in the die array, rolls it, and prints it out in the terminal.
         for(int i = 0; i<50; i++){
-            dice[i].setNumSides(6);
+            dice[i] = new Die(6);
             dice[i].roll();
-            System.out.println("Roll " + i + ": " + dice[i].readLastRoll());
+            System.out.println("Roll " + (i+1) + ": " + dice[i].readLastRoll());
         }
+        //gets the last roll of each die in the array and adds it to totalDiceValue
         for(int i = 0; i<50; i++){
             totalDiceValue += dice[i].readLastRoll();
         }
-        System.out.println("Total value of the " + dice.length + " rolls is " + totalDiceValue);
+        //prints out totalDiceValue
+        System.out.println("Total value of the " + (dice.length) + " rolls is " + totalDiceValue);
     }
 }
